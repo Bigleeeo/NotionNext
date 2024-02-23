d0f5440bf274a4781a05e723941061da5ee7e7cf
Bigleeeo <140473787+Bigleeeo@users.noreply.github.com>
2024-02-23 15:45:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '摄影师一个小胖墩儿', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '让一切的成长都有迹可循，对所有的热爱都全力以赴，Let's Go!！', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '让一切的成长都有迹可循，对所有的热爱都全力以赴', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://摄影师一个小胖墩儿.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
